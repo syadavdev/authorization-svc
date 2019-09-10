8e5d730558b672d1a3736a8bf4c3f2db18fcfbb9
Sandeep <sandeep.yadav@noosyntech.in>
2019-09-11 02:06:12 +0530

    Add KeyStore File For Aurthorization Server Configuration
    Test Case Are Still Failing

--- src/main/java/com/sandi/authorizationsvc/config/AuthorizationServerConfig.java
+++ src/main/java/com/sandi/authorizationsvc/config/AuthorizationServerConfig.java
@@ -1,20 +1,25 @@
 package com.sandi.authorizationsvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
 
 import javax.annotation.PostConstruct;
 import java.security.KeyPair;
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
@@ -25,6 +30,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
 
     private KeyPair keyPair;
 
@@ -38,13 +47,23 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     }
 
     @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception{
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        enhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter()));
+        endpoints.tokenStore(tokenStore())
+                 .accessTokenConverter(accessTokenConverter())
+                 .tokenEnhancer(enhancerChain)
+                 .authenticationManager(authenticationManager);
+    }
+
+    @Override
     public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception{
         oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
     }
 
     @Bean
     public TokenStore tokenStore(){
-        return new JwkTokenStore(accessTokenConverter());
+        return new JwtTokenStore(accessTokenConverter());
     }
 
     @Bean
--- src/main/java/com/sandi/authorizationsvc/config/SecurityConfig.java
+++ src/main/java/com/sandi/authorizationsvc/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.sandi.authorizationsvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void globalUserDetails(final AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManager();
     }
 
 }
