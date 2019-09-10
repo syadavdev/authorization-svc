bcbb1e96b5b20c02a9d187848a2a8d0fbda3b353
Sandeep Yadav <sandi10.96@gmail.com>
2019-09-10 17:28:48 +0530

    Change Java Key Store File Configuration

--- src/main/java/com/sandi/authorizationsvc/config/AuthorizationServerConfig.java
+++ src/main/java/com/sandi/authorizationsvc/config/AuthorizationServerConfig.java
@@ -1,11 +1,56 @@
 package com.sandi.authorizationsvc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+
+import javax.annotation.PostConstruct;
+import java.security.KeyPair;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
-    
+
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Autowired
+    private Environment env;
+
+    private KeyPair keyPair;
+
+    @PostConstruct
+    private void init(){
+        final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
+              ctx.getResource(env.getProperty("oauth2.jwt.jks")),
+              env.getProperty("oauth2.jwt.keypass").toCharArray()
+        );
+        keyPair = keyStoreKeyFactory.getKeyPair(env.getProperty("oauth2.jwt.keypair"));
+    }
+
+    @Override
+    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception{
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new JwkTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setKeyPair(keyPair);
+        return converter;
+    }
 }
